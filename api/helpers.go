5b8e7b64fb8be1e4c6fd13ac8d048ed00c9f0a1f
Riantsoa Lahatra <250556426+lahatraR@users.noreply.github.com>
2026-05-27 09:58:19 +0200

    app mobile

--- api/helpers.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"crypto/rand"
-	"encoding/hex"
-)
-
-func cryptoRandRead(b []byte) (int, error) { return rand.Read(b) }
-func encodeHexStr(b []byte) string          { return hex.EncodeToString(b) }
